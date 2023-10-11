525b665941430d6a953ff730533073a0fa03183a
alberto8812 <cvelascosaavedra@gmail.com>
2023-10-10 22:32:02 -0500

    implement flat list

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { FlatList } from 'react-native';
 import { Text, View } from 'react-native'
 import Icon from "react-native-vector-icons/Ionicons";
+import { styles } from '../theme/appTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface MenuItem{
     name:string,
@@ -15,11 +17,18 @@ const menuItem=[
         name:'Animation 101',
         icon:'cube-outline',
         components:'Animation101Screen'
-    }
+    },
+    {
+        name:'Animation 102',
+        icon:'albumsn-outline',
+        components:'Animation101Screen'
+    },
 ]
 
 export const HomeScreen = () => {
   
+const {top}=useSafeAreaInsets()
+
     const renderMeniItem=(menuItem:MenuItem)=>{
         return(
             <View>
@@ -27,13 +36,38 @@ export const HomeScreen = () => {
             </View>
         )
     }
+    
+    const rederListHeader=()=>{
+
+        return(
+            <View style={{marginTop:top+20,marginBottom:20}}>
+                 <Text style={styles.title}>opciones de menu</Text>
+            </View>
+                 )
+    }
+
+    const itemSeparator=()=>{
+        return(
+            <View
+                style={{
+                    borderBottomWidth:1,
+                    opacity:0.4,
+                    marginVertical:8
+                }}
+            />
+        )
+    }
 
     return (
-    <View style={{flex:1}}>
+    <View style={{flex:1, ...styles.globalMargin}}>
+
+        
         <FlatList
             data={menuItem}
             renderItem={({item})=>renderMeniItem(item)}
             keyExtractor={(item)=>item.name}
+            ListHeaderComponent={()=>rederListHeader()}
+            ItemSeparatorComponent={()=>itemSeparator()}
         />
     </View>
   )
