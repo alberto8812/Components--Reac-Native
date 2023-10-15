055ebaebb8849a6e93b2dd8e3990c911f1e7a4a0
alberto8812 <cvelascosaavedra@gmail.com>
2023-10-15 17:46:42 -0500

    use animation timing

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,14 +1,49 @@
 import React, { useRef, useState } from 'react'
-import { Animated, StyleSheet, View } from 'react-native'
+import { Animated, Button, StyleSheet, View } from 'react-native'
 
 
 export const Animation101Screen = () => {
 
 const opacity = useRef(new Animated.Value(0.4)).current;// value maneja un solo valor , x y y  manejar dos valores 
+ 
+const fadeIn=()=>{
+  Animated.timing(
+    opacity,
+    {
+      toValue:1,//valor a que necesito que llegue el valor
+      duration:300,//duraacion
+      useNativeDriver:true,// animacion acelerada por hardware
+    }
 
-  return (
+  ).start();// realiza una animacion acorde aun tiempo debe ser de animated value
+}
+
+const fadeOut=()=>{
+  Animated.timing(
+    opacity,
+    {
+      toValue:0.4,
+      duration:300,
+      useNativeDriver:true
+    }
+  ).start();
+}
+
+return (
     <View style={styles.container}>
-        <Animated.View style={{...styles.purpleBox,opacity:opacity}}/>
+        <Animated.View style={{...styles.purpleBox,opacity:opacity,marginBottom:20}}/>
+       <View
+       style={{}}
+       >
+        <Button 
+        title='Fade IN'
+        onPress={fadeIn}        
+        />
+        <Button 
+        title='Fade out'
+        onPress={fadeOut}        
+        />
+       </View>
     </View>
   )
 }
