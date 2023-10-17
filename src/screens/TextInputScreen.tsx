8cbe66b8945bd2e7ce85ae3d076a7d786f717170
alberto8812 <cvelascosaavedra@gmail.com>
2023-10-17 17:12:38 -0500

    down board

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
+import { Keyboard, KeyboardAvoidingView, StyleSheet, TextInput, TouchableWithoutFeedback, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 import { HeaderTitle } from '../components/HeaderTitle'
+import { ScrollView } from 'react-native-gesture-handler'
 
 
 
@@ -29,37 +30,43 @@ export const TextInputScreen = () => {
     }
 
   return (
-    <View style={styles.globalMargin}>
-      <HeaderTitle title='TextInput'/>
+    <KeyboardAvoidingView>
+      <ScrollView>
+       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.globalMargin}>
+                <HeaderTitle title='TextInput'/>
 
-      <TextInput 
-         style={stylesScreen.TextInpuStyle}
-         placeholder='ingrese number'
-         autoCorrect={false}// para que no realice correcciones 
-         value={form.name}
-         autoCapitalize='words'// capitalizar palabras
-         onChangeText={(value)=>TextOnchange(value,'name')}
-      />
-      <TextInput 
-         style={stylesScreen.TextInpuStyle}
-         autoCorrect={false}// para que no realice correcciones 
-         placeholder='ingrese su email'
-         autoCapitalize='none'// not capitalizar palabras
-         value={form.email}
-         onChangeText={(value)=>TextOnchange(value,'email')}
-        keyboardType='email-address'//definimos las caracteristicas que traiga el teclado ejemplo @
-      />
-      <TextInput 
-         style={stylesScreen.TextInpuStyle}
-         autoCorrect={false}// para que no realice correcciones 
-         placeholder='ingrese su telefono'
-         autoCapitalize='none'// capitalizar palabras
-         value={form.phone}
-         onChangeText={(value)=>TextOnchange(value,'phone')}
-         keyboardType='phone-pad'
-      />
-        <HeaderTitle title={JSON.stringify(form,null,3)}/>
-    </View>
+                <TextInput 
+                    style={stylesScreen.TextInpuStyle}
+                    placeholder='ingrese number'
+                    autoCorrect={false}// para que no realice correcciones 
+                    value={form.name}
+                    autoCapitalize='words'// capitalizar palabras
+                    onChangeText={(value)=>TextOnchange(value,'name')}
+                />
+                <TextInput 
+                    style={stylesScreen.TextInpuStyle}
+                    autoCorrect={false}// para que no realice correcciones 
+                    placeholder='ingrese su email'
+                    autoCapitalize='none'// not capitalizar palabras
+                    value={form.email}
+                    onChangeText={(value)=>TextOnchange(value,'email')}
+                    keyboardType='email-address'//definimos las caracteristicas que traiga el teclado ejemplo @
+                />
+                <TextInput 
+                    style={stylesScreen.TextInpuStyle}
+                    autoCorrect={false}// para que no realice correcciones 
+                    placeholder='ingrese su telefono'
+                    autoCapitalize='none'// capitalizar palabras
+                    value={form.phone}
+                    onChangeText={(value)=>TextOnchange(value,'phone')}
+                    keyboardType='phone-pad'
+                />
+                <HeaderTitle title={JSON.stringify(form,null,3)}/>
+            </View>
+      </TouchableWithoutFeedback>
+        </ScrollView>   
+    </KeyboardAvoidingView>
   )
 }
 
